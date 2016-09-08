398bff3ef63ed8266d314a9bbfe74ef6c2d190d0
John van den Berg <john44berg@gmail.com>
2016-09-08 14:47:19 +0200

    Implemented platform spawning and stacking

--- gamejame1_2d/Assets/scripts/CameraController.cs
+++ gamejame1_2d/Assets/scripts/CameraController.cs
@@ -1,15 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking.NetworkSystem;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+    private static CameraController _instance;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static CameraController Instance
+    {
+        get { return _instance; }
+    }
+
+    [SerializeField] private float speed = 2f;
+
+    private float cameraHeight;
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        cameraHeight = transform.position.y;
+    }
+
+    void Update()
+    {
+        float f = Mathf.Lerp(transform.position.y, cameraHeight, Time.deltaTime*speed);
+        transform.position = new Vector3(transform.position.x, f, transform.position.z);
+    }
+
+    public void IncreaseCameraHeight(float val)
+    {
+        cameraHeight += val;
+        Debug.Log("Increasing camera height to: " + cameraHeight);
+    }
 }
