01581d00ce4034cd96704fad7810b35a6b4bac8f
Joris van Willigen <joris@labela.nl>
2016-09-29 15:15:26 +0200

    start score ui

---
+++ Assets/Scripts/PopupController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupController : MonoBehaviour
+{
+
+    private static FloatingPopupText popupText;
+    private static GameObject canvas;
+
+    public static void Initialize()
+    {
+        canvas = GameObject.Find("canvas");
+        if (!popupText)
+            popupText = Resources.Load<FloatingPopupText>("Prefabs/PopupTextParent");
+    }
+
+    public static void CreateFloatingText(string text)
+    {
+        FloatingPopupText instance = Instantiate(popupText);
+        instance.transform.SetParent(canvas.transform, false);
+        instance.setText(text);
+    }
+}
