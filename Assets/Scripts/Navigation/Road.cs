0d1f907a6100ae939847a112a809e1029bc1b0ef
John van den Berg <John44berg@gmail.com>
2016-11-22 15:02:59 +0100

    Feature dynamic world (#23)
    * Implemented foundation for custom editor
    
    * Implemented automated road finding
    Implemented automated navigable object conversion
    Implemented clear functionality
    
    * Implemented basic waypoint generation
    Implemented debugging visualization
    
    * Implemented waypoint position calculations for lanes
    
    * Added safety to prevent excess waypoints
    
    * Adjusted implementation due to unity limitations
    
    * Implemented automated waypoint calculation for lanes, sections and corners
    
    * Rebuil waypoint editor
    
    * Added particle trails
    
    * Cleaned demolition scene
    
    * Pull request changes

---
+++ Assets/Scripts/Navigation/Road.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public abstract class Road : MonoBehaviour
+{
+    public float waypointOffset = -1.5f;
+
+    protected List<Waypoint> waypoints = new List<Waypoint>();
+
+    // Debugging
+    private bool debug = false;
+    private float gizmoRadius = 0.5f;
+    private Color gizmoColor = Color.red;
+
+    public enum RoadType
+    {
+        Lane, Corner, Intersection, Tintersection, DeadEnd
+    }
+    
+    void OnDrawGizmos()
+    {
+        if (!debug)
+        {
+            return;
+        }
+
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (waypoint == null)
+                return;
+
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawSphere(waypoint.transform.position, gizmoRadius);
+        }
+    }
+
+    public abstract RoadType GetRoadType();
+    public abstract int WaypointLimit();
+    public abstract void CalculateWaypointPosition();
+
+    public void AddWaypoint(Waypoint waypoint)
+    {
+        waypoints.Add(waypoint);
+    }
+
+    public void ClearWaypoints()
+    {
+        // Uncomment in case of a bug when removing children
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            DestroyImmediate(child.gameObject);
+        }
+
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (waypoint == null)
+                continue;
+
+            DestroyImmediate(waypoint.gameObject);
+        }
+
+        waypoints.Clear();
+    }
+
+    public void SetDebugging(bool debug)
+    {
+        this.debug = debug;
+        print("Debugging " + (debug ? "enabled" : "disabled"));
+    }
+
+    public void SetDebugging(Color gizmoColor, float gizmoRadius)
+    {
+        this.gizmoColor = gizmoColor;
+        this.gizmoRadius = gizmoRadius;
+    }
+
+    public void SetDebugging(bool debug, Color gizmoColor, float gizmoRadius)
+    {
+        SetDebugging(debug);
+        this.gizmoColor = gizmoColor;
+        this.gizmoRadius = gizmoRadius;
+    }
+
+    public int GetWaypointCount()
+    {
+        return waypoints.Count;
+    }
+
+    protected void PositionWaypointCenter(Waypoint w1, Waypoint w2)
+    {
+        float centerOffset = GetQuarter(GetMeshWidth());
+
+        PositionWaypointDuo(w1, w2, transform.position, Vector3.right, centerOffset);
+    }
+
+    protected void PositionWaypointForward(Waypoint w1, Waypoint w2)
+    {
+        float centerOffset = GetQuarter(GetMeshWidth());
+
+        w1.gameObject.name = w1.gameObject.name + "Forward" + 1;
+        w2.gameObject.name = w2.gameObject.name + "Forward" + 2;
+
+        Vector3 position = GetPosition(transform.forward, GetMeshLength());
+        PositionWaypointDuo(w1, w2, position, Vector3.right, centerOffset);
+    }
+
+    protected void PositionWaypointBack(Waypoint w1, Waypoint w2)
+    {
+        float centerOffset = GetQuarter(GetMeshWidth());
+
+        w1.gameObject.name = w1.gameObject.name + "Back" + 1;
+        w2.gameObject.name = w2.gameObject.name + "Back" + 2;
+
+        Vector3 position = GetPosition(transform.forward * -1, GetMeshLength());
+        PositionWaypointDuo(w1, w2, position, Vector3.right, centerOffset);
+    }
+
+    protected void PositionWaypointRight(Waypoint w1, Waypoint w2)
+    {
+        float centerOffset = GetQuarter(GetMeshLength());
+
+        w1.gameObject.name = w1.gameObject.name + "Right" + 1;
+        w2.gameObject.name = w2.gameObject.name + "Right" + 2;
+
+        Vector3 position = GetPosition(transform.right, GetMeshLength());
+        PositionWaypointDuo(w1, w2, position, Vector3.forward, centerOffset);
+    }
+
+    protected void PositionWaypointLeft(Waypoint w1, Waypoint w2)
+    {
+        float centerOffset = GetQuarter(GetMeshLength());
+
+        w1.gameObject.name = w1.gameObject.name + "Left" + 1;
+        w2.gameObject.name = w2.gameObject.name + "Left" + 2;
+
+        Vector3 position = GetPosition(transform.right * -1, GetMeshLength());
+        PositionWaypointDuo(w1, w2, position, Vector3.forward, centerOffset);
+    }
+
+    private Vector3 GetPosition(Vector3 direction, float size)
+    {
+        return transform.position + (direction * (size / 2));
+    }
+
+    private void PositionWaypointDuo(Waypoint w1, Waypoint w2, Vector3 position, Vector3 offsetDirection, float centerOffset)
+    {
+        Vector3 translation = offsetDirection * (centerOffset + waypointOffset);
+
+        w1.transform.position = position;
+        w2.transform.position = position;
+
+        w1.transform.Translate(translation, gameObject.transform);
+        w2.transform.Translate(-translation, gameObject.transform);
+    }
+
+    private float GetQuarter(float val)
+    {
+        return val / 4;
+    }
+
+    private float GetMeshWidth()
+    {
+        return GetMeshSize().x;
+    }
+
+    private float GetMeshLength()
+    {
+        return GetMeshSize().z;
+    }
+
+    private Vector3 GetMeshSize()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        return mesh.bounds.size;
+    }
+}
---
+++ Assets/Scripts/Utility/Gizmo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gizmo : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 2f;
+
+    [SerializeField]
+    private Color color = Color.blue;
+
+    [SerializeField]
+    private Shape shape = Shape.Cirle;
+
+    private enum Shape
+    {
+        Cirle,
+        Cube
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        switch (shape)
+        {
+            case Shape.Cirle:
+                Gizmos.DrawWireSphere(transform.position, radius);
+                break;
+            case Shape.Cube:
+                Gizmos.DrawCube(transform.position, new Vector3(radius, radius, radius));
+                break;
+        }
+    }
+}
